1b74e309267bf0a2ee738e2c7a49be57a4cea64b
fomka94 <fom-aina@mail.ru>
2022-05-17 21:03:57 +0900

    Initial Commit

---
+++ timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  timer
+//
+//  Created by Фомина Айина Валерьевна on 17.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
